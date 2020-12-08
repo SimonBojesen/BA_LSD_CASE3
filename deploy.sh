7c5c16ee069676983cbcc1a548be5e27fad63998
Kenneth L. Hansen <klh@minuba.dk>
2020-12-08 21:47:06 +0100

    testing deployment

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 scp booking-service.service root@207.154.197.222:/etc/systemd/system
 scp target/BA_LSD_CASE3-1.0-SNAPSHOT.jar root@207.154.197.222:/artifact
+systemctl start booking-service.service
